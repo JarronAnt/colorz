2c4ff8d7169414230d55e0ee4a91dd094804364f
JarronAnt <jarronblizzard@gmail.com>
2023-10-06 11:04:39 -0400

    add checkerboard

--- demo.c
+++ demo.c
@@ -2,9 +2,16 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
+#include <stdbool.h>
 
 #define WIDTH 800
 #define HEIGHT 600
+#define COLS (8*2)
+#define ROWS (6*2)
+#define CELL_WIDTH  (WIDTH/COLS)
+#define CELL_HEIGHT (HEIGHT/ROWS)
+#define BACKGROUND_COLOR 0xFF202020
+#define FOREGROUND_COLOR 0xFF2020FF
 
 #define returnDefer(val) do {res = (val); goto defer;} while(0)
 
@@ -12,32 +19,6 @@
 static uint32_t pixels[WIDTH*HEIGHT];
 
 typedef int Errno;
-
-void colorz_fill(uint32_t* pixels, size_t w, size_t h, uint32_t color){
-
-    for(size_t i = 0 ; i < w*h ; ++i){
-       pixels[i] = color;
-    }
-}
-
-void colorz_fillRect(uint32_t *pixels, size_t pixelsWidth, size_t pixelsHeight,
-                      int x0, int y0, size_t w, size_t h,
-                      uint32_t color)
-{
-    //Loop through the entire canvas and if its within our rect bounds assign the pixel a color
-    for (int dy = 0; dy < (int) h; ++dy) {
-        int y = y0 + dy;
-        if (0 <= y && y < (int) pixelsHeight) {
-            for (int dx = 0; dx < (int) w; ++dx) {
-                int x = x0 + dx;
-                if (0 <= x && x < (int) pixelsWidth) {
-                    pixels[y*pixelsWidth + x] = color;
-                }
-            }
-        }
-    }
-}
-
 
 Errno colorz_saveToPPM(uint32_t* pixels, size_t w, size_t h, const char* filePath){
 
@@ -67,16 +48,76 @@ Errno colorz_saveToPPM(uint32_t* pixels, size_t w, size_t h, const char* filePat
         }
     }
 
-
 defer:
     if(file) fclose(file);
     return res;
+    
 }
+
+void colorz_fill(uint32_t* pixels, size_t w, size_t h, uint32_t color){
+
+    for(size_t i = 0 ; i < w*h ; ++i){
+       pixels[i] = color;
+    }
+}
+
+void colorz_fillRect(uint32_t *pixels, size_t pixelsWidth, size_t pixelsHeight,
+                      int x0, int y0, size_t w, size_t h,
+                      uint32_t color)
+{
+    //Loop through the entire canvas and if its within our rect bounds assign the pixel a color
+    for (int dy = 0; dy < (int) h; ++dy) {
+        int y = y0 + dy;
+        if (0 <= y && y < (int) pixelsHeight) {
+            for (int dx = 0; dx < (int) w; ++dx) {
+                int x = x0 + dx;
+                if (0 <= x && x < (int) pixelsWidth) {
+                    pixels[y*pixelsWidth + x] = color;
+                }
+            }
+        }
+    }
+}
+
+bool checkerBoard(void)
+{
+    //fill the whole canvas with bg color
+    colorz_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
+
+    for (int y = 0; y < ROWS; ++y) {
+        for (int x = 0; x < COLS; ++x) {
+            uint32_t color = BACKGROUND_COLOR;
+            //if we are in an even numbered cell change the color 
+            if ((x + y)%2 == 0) {
+                color = 0xFF2020FF;
+            }
+            //fill the cells with the alternating colors 
+            colorz_fillRect(pixels, WIDTH, HEIGHT,
+            x*CELL_WIDTH, y*CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, color);
+        }
+    }
+
+    const char *filePath = "checker.ppm";
+    Errno err = colorz_saveToPPM(pixels, WIDTH, HEIGHT, filePath);
+
+    if (err) {
+        return false;
+    }
+
+    return true;
+}
+
+float lerpf(float a, float b, float t)
+{
+    return a + (b - a)*t;
+}
+
 
 int main(){
 
     //colorz_fill(pixels, WDITH, HEIGHT, 0xFF00FFFF); //BGR
-    colorz_fillRect(pixels,WIDTH, HEIGHT, 25,25,75,75,0xE66B3D);
+    //colorz_fillRect(pixels,WIDTH, HEIGHT, 25,25,75,75,0xE66B3D);
+    checkerBoard();
     const char* filePath = "out.ppm";
     Errno err = colorz_saveToPPM(pixels, WIDTH, HEIGHT, filePath);
     if(err) {
